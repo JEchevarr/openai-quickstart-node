73be792d7b2f485412443bc5e3b8e719da2251f5
JEchevarr <ignacio.echevarria@iugo.com.uy>
2023-04-26 10:15:17 -0300

    message

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,19 +19,21 @@ export default async function (req, res) {
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Ask Batman!",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ "role": "system", "content": "From now on answer as if you are Batman." },
+      {"role": "user", "content": animal }]
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    
+    res.status(200).json({ result: completion.data.choices[0].message.content });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
